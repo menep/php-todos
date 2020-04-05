9702e19a2d7bfa852c9da076dea88aac3fb0daf3
menep <menep@protonmail.com>
2020-04-05 15:10:32 +0200

    add 'url' method to Request class

--- app/Request.php
+++ app/Request.php
@@ -1,7 +1,14 @@
 <?php
 
-class Request {
-    public static function method() {
+class Request
+{
+    public static function method()
+    {
         return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public static function url()
+    {
+        return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     }
 }
