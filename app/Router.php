cd0bee00072ddb9d2b27e5145499855f6918904e
menep <menep@protonmail.com>
2020-04-05 15:43:00 +0200

    create Router class

---
+++ app/Router.php
@@ -0,0 +1,37 @@
+<?php
+
+require '../app/Request.php';
+require '../app/controllers/HomeController.php';
+
+class Router
+{
+    protected $routes = [
+        'GET' => [
+            '/' => 'HomeController@index',
+            'todos' => 'TodoController@index',
+        ],
+        'POST' => [
+            '/todos/create' => 'TodoController@store'
+        ]
+    ];
+
+    public function direct()
+    {
+        $url = Request::url();
+        $method = Request::method();
+        
+        if (array_key_exists($url, $this->routes[$method])) {
+            [$controllerName, $controllerMethod] = explode('@', $this->routes[$method][$url]);
+            self::executeController($controllerName, $controllerMethod);
+        }
+    }
+
+    private static function executeController($controllerName, $method)
+    {
+        if (file_exists('../app/controllers/' . $controllerName . '.php')) {
+            (new $controllerName)->$method();
+        } else {
+            echo 'File not found';
+        }
+    }
+}
