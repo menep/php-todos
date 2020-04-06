39d0344cd5a3cea68eb5945dd583e11224ef9ab2
menep <menep@protonmail.com>
2020-04-06 09:55:07 +0200

    fix accessing 'DOCUMENT_ROOT' as string

--- app/controllers/MiscController.php
+++ app/controllers/MiscController.php
@@ -3,12 +3,13 @@
 class MiscController {
     public function index()
     {
-        require $_SERVER[DOCUMENT_ROOT] . '/index.html';
+        
+        require $_SERVER['DOCUMENT_ROOT'] . '/index.html';
     }
 
     public function notFound()
     {
         header("HTTP/1.0 404 Not Found");
-        require $_SERVER[DOCUMENT_ROOT] . '/not-found.html';
+        require $_SERVER['DOCUMENT_ROOT'] . '/not-found.html';
     }
 }
