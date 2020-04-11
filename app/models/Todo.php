34df90bd4bd4ac8f44ffcdff5a80c9dbd4c9fde5
menep <menep@protonmail.com>
2020-04-11 10:05:40 +0200

    Todo model: use 'Validator' service in 'save' method

--- app/models/Todo.php
+++ app/models/Todo.php
@@ -4,17 +4,26 @@ namespace App\Models;
 
 use App\Core\Request;
 use App\Core\Database;
+use App\Services\Validator;
 
 class Todo
 {
-    public static function save()
+    public static function save($todo)
     {
-        $parameters = Request::payload();
-        $boundParameters = [
-            'body' => $parameters['body'],
-            'user_id' => 1 // TODO: set up registration or assignment of todos
-        ];
-        $sql = 'INSERT INTO todos (body, user_id) values (:body, :user_id)';
-        (new Database)->query($sql, $boundParameters);
+        try {
+            $validationCriteria = ['body' => 'required'];
+            Validator::validate($todo, $validationCriteria);
+
+            // $boundParameters = [
+            // 'body' => $parameters['body'],
+            // 'user_id' => 1 // TODO: set up registration or assignment of todos
+            // ];
+
+            // $sql = 'INSERT INTO todos (body, user_id) values (:body, :user_id)';
+
+            // (new Database)->query($sql, $boundParameters);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
