18e20b8f0bfa09e2524f82ef43f99eab09753401
menep <menep@protonmail.com>
2020-04-16 08:48:31 +0200

    Todo: remove validation logic from 'save' method, add simple query functionality

--- app/models/Todo.php
+++ app/models/Todo.php
@@ -11,21 +11,14 @@ class Todo
     public static function save($todo)
     {
         try {
-            $validationCriteria = ['body' => 'required,string,maxChar255'];
-            $errors = (new Validator)->validate($todo, $validationCriteria);
-            if (!empty($errors)) {
-                throw new Exception($errors, 1);
-            }
-            // $boundParameters = [
-            // 'body' => $parameters['body'],
-            // 'user_id' => 1 // TODO: set up registration or assignment of todos
-            // ];
+            // TODO: add validation
 
-            // $sql = 'INSERT INTO todos (body, user_id) values (:body, :user_id)';
+            $sql = 'INSERT INTO todos (body, priority) values (:body, :priority)';
 
-            // (new Database)->query($sql, $boundParameters);
+            (new Database)->query($sql, ['body' => $todo['body'], 'priority' => $todo['priority']]);
         } catch (\Throwable $th) {
-            throw $th;
+            var_dump($th);
+            die();
         }
     }
 }
