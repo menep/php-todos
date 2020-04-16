0bfa86b29ddde00bc438689d3ebf08b37c8c394d
menep <menep@protonmail.com>
2020-04-16 09:35:22 +0200

    create.todos view: change file extension to 'php', show success message through session data

---
+++ public/create.todos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Php todos</title>
+</head>
+
+<body>
+  <h1>Create todo</h1>
+  <form action="/todos/create" method="POST">
+    <label>Body: <input type="text" name="body" /></label>
+    <br />
+    <label>Priority:
+      <select name="priority">
+        <option value="1">High</option>
+        <option value="2">Normal</option>
+        <option value="3">Low</option>
+      </select>
+    </label>
+    <br />
+    <input type="submit" value="Create todo" />
+  </form>
+
+  <?php if ($_SESSION['success']) : ?>
+  <p style="color: green;">Your new Todo was successfully created!</p>
+  <?php endif ?>
+</body>
+
+</html>
