cd9206e38fdf46387507ab4ce6826995349058d5
menep <menep@protonmail.com>
2020-04-17 14:37:07 +0200

    index.todos view: change list layout to table

--- resources/views/todos/index.todos.php
+++ resources/views/todos/index.todos.php
@@ -1,14 +1,30 @@
 <h1>All todos</h1>
 
 <?php if ($fetchedTodos) : ?>
-<ul>
-    <?php foreach ($fetchedTodos as $todo) : ?>
-    <li>
-        <?= $todo['id'] . ', ' . $todo['body'] ?>
-    </li>
-
-    <?php endforeach ?>
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Todo</th>
+            <th>Created at</th>
+            <th>Priority</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($fetchedTodos as $todo) : ?>
+        <tr>
+            <td><?= $todo['id'] ?>
+            </td>
+            <td><?= $todo['body'] ?>
+            </td>
+            <td><?= $todo['due'] ?>
+            </td>
+            <td><?= $todo['priority'] ?>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
 
 <?php else : ?>
 <p>No todos found</p>
