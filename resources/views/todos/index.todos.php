ffdef9c4edd4c50aa1c1f224376039119dac35c5
menep <menep@protonmail.com>
2020-04-17 09:30:28 +0200

    index.todos view: Add control structure in case no todos are retrieved

--- resources/views/todos/index.todos.php
+++ resources/views/todos/index.todos.php
@@ -1,9 +1,16 @@
 <h1>All todos</h1>
 
+<?php if ($fetchedTodos) : ?>
 <ul>
     <?php foreach ($fetchedTodos as $todo) : ?>
-
-    <li><?= $todo['id'] . ', ' . $todo['body'] ?></li>
+    <li>
+        <?= $todo['id'] . ', ' . $todo['body'] ?>
+    </li>
 
     <?php endforeach ?>
 </ul>
+
+<?php else : ?>
+<p>No todos found</p>
+
+<?php endif;
