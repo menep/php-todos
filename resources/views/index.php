a651f364719fd811922a69e6a5fa0881463150cd
menep <menep@protonmail.com>
2020-04-19 11:12:05 +0200

    index.php: move logic

---
+++ resources/views/index.php
@@ -0,0 +1,7 @@
+<?php require __DIR__ . '/partials/head.html'; ?>
+
+    <h1>Php todos</h1>
+    <a href="/todos">View all todos</a>
+    <a href="/todos/create">Create a todo</a>
+  </body>
+</html>
