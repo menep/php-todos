f36194d723d6823aba28226304c094dc3fc17b33
menep <menep@protonmail.com>
2020-04-19 12:02:05 +0200

    db-init: update to use Query class

--- utilities/db-init.php
+++ utilities/db-init.php
@@ -1,32 +1,13 @@
 <?php
 require 'vendor/autoload.php';
 
+use App\Core\Query;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-$host = getenv('HOST');
-$db = getenv('DBNAME');
-$port = getenv('PORT');
-
-$dsn = "mysql:host={$host};dbname={$db};port={$port};charset=utf8mb4";
-
-$user = getenv('DBUSER');
-$password = getenv('DBPASSWORD');
-
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-try {
-    $pdo = new \PDO($dsn, $user, $password, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
 
 $users = file_get_contents(__DIR__ . '/users.sql');
 $todos = file_get_contents(__DIR__ . '/todos.sql');
 
-$pdo->query($users);
-$pdo->query($todos);
+Query::execute($users);
+Query::execute($todos);
