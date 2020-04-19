c999a6f91679c304705191bfe06c6ac1710048d9
menep <menep@protonmail.com>
2020-04-19 11:07:30 +0200

    MiscController: update paths

--- app/controllers/MiscController.php
+++ app/controllers/MiscController.php
@@ -6,12 +6,12 @@ class MiscController
 {
     public function index()
     {
-        require $_SERVER['DOCUMENT_ROOT'] . '/index.html';
+        require '../resources/views/index.html';
     }
 
     public function notFound()
     {
         header("HTTP/1.0 404 Not Found");
-        require $_SERVER['DOCUMENT_ROOT'] . '/not-found.html';
+        require '../resources/views/not-found.html';
     }
 }
