b5b0c10c9920ea2faff8d1f7150b939585db840a
menep <menep@protonmail.com>
2020-04-21 11:19:01 +0200

    MiscController: update methods to use $contentPath

--- app/controllers/MiscController.php
+++ app/controllers/MiscController.php
@@ -6,13 +6,14 @@ class MiscController
 {
     public function index()
     {
-        $content = file_get_contents(__DIR__ . '/../../resources/views/index.html');
+        $contentPath = \getcwd() . '/../resources/views/index.html';
+        
         require __DIR__ . '/../../resources/views/partials/base.view.php';
     }
 
     public function notFound()
     {
-        $content = file_get_contents(__DIR__ . '/../../resources/views/not-found.html');
+        $contentPath = \getcwd() . '/../resources/views/not-found.html';
         
         header("HTTP/1.0 404 Not Found");
         require __DIR__ . '/../../resources/views/partials/base.view.php';
