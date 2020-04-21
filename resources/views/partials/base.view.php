0bca8a06c383caf6a714c6f97e4ae9d1474c5d43
menep <menep@protonmail.com>
2020-04-21 11:18:24 +0200

    base.view: require file from path passed through $contentPath

--- resources/views/partials/base.view.php
+++ resources/views/partials/base.view.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Php todos</title>
-    <link rel="stylesheet" href="/styles/base.css" />
-    <link rel="stylesheet" href="/styles/header.css" />
-  </head>
 
-  <body>
-    <div class="app-wrapper">
-      <header class="header">
-        <nav>
-          <ul class="header--list">
-            <li class="header--list-item"><a href="/">Home</a></li>
-            <li class="header--list-item"><a href="/todos">Todo list</a></li>
-            <li class="header--list-item">
-              <a href="/todos/create">Create todo</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Php todos</title>
+  <link rel="stylesheet" href="/styles/base.css" />
+  <link rel="stylesheet" href="/styles/header.css" />
+</head>
 
-      <?php echo $content; ?>
+<body>
+  <div class="app-wrapper">
+    <header class="header">
+      <nav>
+        <ul class="header--list">
+          <li class="header--list-item"><a href="/">Home</a></li>
+          <li class="header--list-item"><a href="/todos">Todo list</a></li>
+          <li class="header--list-item">
+            <a href="/todos/create">Create todo</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
 
-    </div>
-  </body>
+    <?php require $contentPath; ?>
+
+  </div>
+</body>
+
 </html>
