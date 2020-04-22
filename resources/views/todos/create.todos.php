6c7984d9b2d4452790e5790b814aa09c4f94d7d2
menep <menep@protonmail.com>
2020-04-22 10:26:29 +0200

    create.todos: update layout

--- resources/views/todos/create.todos.php
+++ resources/views/todos/create.todos.php
@@ -1,26 +1,31 @@
-<h1>Create todo</h1>
-<form action="/todos/create" method="POST">
-  <label>Body: <input type="text" name="body" /></label>
-  <br>
-  <label>Due:
-    <input type="date" name="due">
-  </label>
-  <br />
-  <label>Priority:
-    <select name="priority">
-      <option value="1">High</option>
-      <option value="2">Normal</option>
-      <option value="3">Low</option>
-    </select>
-  </label>
-  <br />
-  <input type="submit" value="Create todo" />
-</form>
+<main class="flex-container flex-column flex-align-center margin-top-lg">
+  <h1 class="font-md">Create todo</h1>
+  <section class="margin-top-md">
+    <form action="/todos/create" method="POST">
+      <label>Body: <input type="text" name="body" /></label>
+      <br>
+      <label>Due:
+        <input type="date" name="due">
+      </label>
+      <br />
+      <label>Priority:
+        <select name="priority">
+          <option value="1">High</option>
+          <option value="2">Normal</option>
+          <option value="3">Low</option>
+        </select>
+      </label>
+      <br />
+      <input type="submit" value="Create todo" />
+    </form>
 
-<?php if (isset($_SESSION)) : ?>
-<?php if ($_SESSION['success']) : ?>
-<p style="color: green;">Your new Todo was successfully created!</p>
-<?php elseif ($_SESSION['error']) : ?>
-<p style="color: red;">There was an issue creating your todo</p>
-<?php endif; ?>
-<?php endif;
+    <?php if (isset($_SESSION)) : ?>
+    <?php if ($_SESSION['success']) : ?>
+    <p style="color: green;">Your new Todo was successfully created!</p>
+    <?php elseif ($_SESSION['error']) : ?>
+    <p style="color: red;">There was an issue creating your todo</p>
+    <?php endif; ?>
+    <?php endif;?>
+  </section>
+
+</main>
