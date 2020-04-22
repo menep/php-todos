99291801721292a0e24e94fe150a8e3d272b8184
menep <menep@protonmail.com>
2020-04-22 10:22:39 +0200

    index.todos: update layout

--- resources/views/todos/index.todos.php
+++ resources/views/todos/index.todos.php
@@ -1,39 +1,41 @@
-<main>
-    <h1>All todos</h1>
+<main class="flex-container flex-column flex-align-center margin-top-lg">
+    <h1 class="font-md">All todos</h1>
 
-    <?php if ($fetchedTodos) : ?>
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Todo</th>
-                <th>Created at</th>
-                <th>Priority</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($fetchedTodos as $todo) : ?>
-            <tr>
-                <td><?= $todo['id'] ?>
-                </td>
-                <td>
-                    <a
-                        href="/todos/<?= $todo['id'] ?>">
-                        <?= $todo['body'] ?>
-                    </a>
-                </td>
-                <td><?= $todo['due'] ?>
-                </td>
-                <td><?= $todo['priority'] ?>
-                </td>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+    <section class="margin-top-md">
+        <?php if ($fetchedTodos) : ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Todo</th>
+                    <th>Created at</th>
+                    <th>Priority</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($fetchedTodos as $todo) : ?>
+                <tr>
+                    <td><?= $todo['id'] ?>
+                    </td>
+                    <td>
+                        <a
+                            href="/todos/<?= $todo['id'] ?>">
+                            <?= $todo['body'] ?>
+                        </a>
+                    </td>
+                    <td><?= $todo['due'] ?>
+                    </td>
+                    <td><?= $todo['priority'] ?>
+                    </td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
 
-    <?php else : ?>
-    <p>No todos found</p>
+        <?php else : ?>
+        <p>No todos found</p>
 
-    <?php endif; ?>
+        <?php endif; ?>
+    </section>
 
 </main>
